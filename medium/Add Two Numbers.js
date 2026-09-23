0988bb691c6f37cddafbdb9addc448ddb3dae959
sami-dev-dz <sami.codefree@gmail.com>
2026-09-23 12:54:45 +0100

    Fix variable declaration for sum in addition logic

--- medium/Add Two Numbers.js
+++ medium/Add Two Numbers.js
@@ -34,7 +34,7 @@ var addTwoNumbers = function (l1, l2) {
   let carry = 0;
 
   while (l1 !== null || l2 !== null || carry !== 0) {
-    sum = carry;
+    let sum = carry;
     if (l1 !== null) {
       sum += l1.val;
       l1 = l1.next;
